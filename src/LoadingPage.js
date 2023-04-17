6fb4cee38bfc61518222c4f459dfd8bd281e8054
guoxiaomin <514594349@qq.com>
2023-04-17 15:08:34 +0800

    修改eslint配置

--- src/LoadingPage.js
+++ src/LoadingPage.js
@@ -1,23 +1,24 @@
-
 import React from "react";
 
 const LoadingPage = () => (
-    <div style={{
-        position: `fixed`,
-        top: `0`,
-        left: `0`,
-        width: ` 100%`,
-        height: ` 100%`,
-        background: `#171b22`,
-        zIndex: `2`
-    }}>
+    <div
+        style={{
+            position: `fixed`,
+            top: `0`,
+            left: `0`,
+            width: ` 100%`,
+            height: ` 100%`,
+            background: `#171b22`,
+            zIndex: `2`,
+        }}
+    >
         <a target="_blank" href="/" rel="noreferrer">
             <img
                 style={{
                     position: `absolute`,
                     top: `50%`,
                     left: ` 50%`,
-                    transform: `translate(-50%, -50%)`
+                    transform: `translate(-50%, -50%)`,
                 }}
                 width="100"
                 alt="logo"
