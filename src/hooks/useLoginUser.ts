3bdf881f993da74390367c95142f4d0ba474d811
Alter <514594349@qq.com>
2025-08-14 15:07:26 +0800

    更新useLoginUser.ts文件

--- src/hooks/useLoginUser.ts
+++ src/hooks/useLoginUser.ts
@@ -20,21 +20,8 @@ export const useLoginUser = () => {
         async () => {
             const response = await getUserInfo();
             if (response.status === 200) {
-                if (!response.data) {
-                    // 未登录，跳转到登录页（避免在登录页时重复跳转）
-                    if (window.location.pathname !== '/login') {
-                        navigate('/login');
-                    }
-                    return null;
-                }
                 setLoginUser(response.data);
                 return response.data;
-            } else if (response.status == 401) {
-                // 未登录，跳转到登录页（避免在登录页时重复跳转）
-                if (window.location.pathname !== '/login') {
-                    navigate('/login');
-                }
-                return null;
             } else {
                 throw new Error(response.message || '获取用户信息失败');
             }
